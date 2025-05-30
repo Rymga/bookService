99f56202bca0ee2b700ad5950f905c55add1d6ad
fr <fr.choque@duocuc.cl>
2025-05-30 02:46:33 -0400

    service

--- src/main/java/com/libreriaSanSebastian/bookService/repository/LibroRepository.java
+++ src/main/java/com/libreriaSanSebastian/bookService/repository/LibroRepository.java
@@ -2,10 +2,17 @@ package com.libreriaSanSebastian.bookService.repository;
 
 import com.libreriaSanSebastian.bookService.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
     Libro findByTitulo(String titulo);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Libro l SET l.stock = l.stock - 1 WHERE l.id = :id AND l.stock > 0")
+    int decrementarStock(Long id);
 }
